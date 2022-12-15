4f437defbb45eae816a88c52d19f6776bfad4e9a
Laura0310 <acostavalentina7@gmail.com>
2022-12-15 16:13:31 -0500

    update

--- api/index.js
+++ api/index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+require("dotenv").config();
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => { // este force true coge los modelos y los convierte en tablas
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at',process.env.PORT); // eslint-disable-line no-console
   });
 });
